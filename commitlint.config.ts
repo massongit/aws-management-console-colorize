18db3cef09a2f2149bd16e4c83d02740a2d8fe3c
Masaya Suzuki <suzukimasaya428@gmail.com>
2025-03-09 08:10:19 +0900

    test

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,5 +4,5 @@ module.exports = {
   helpUrl: "https://www.conventionalcommits.org/",
   // We need this until https://github.com/dependabot/dependabot-core/issues/2445
   // is resolved.
-  ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
+  ignores: [(msg: string) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
 };
